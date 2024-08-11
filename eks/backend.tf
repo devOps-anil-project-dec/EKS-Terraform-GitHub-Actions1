a253267c158f75aaccdf7a93100e2f1f98125124
Gowtham Kalipalli <83296469+gowthamkalipalli@users.noreply.github.com>
2024-08-11 16:28:19 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-bucket12"
+    bucket         = "my-ews-bucket123"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
